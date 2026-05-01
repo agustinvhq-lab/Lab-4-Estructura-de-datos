e56bfe744cdd576cef18599cfc5abd3d057c4d3e
Your Name <you@example.com>
2026-05-01 03:34:32 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -103,7 +103,11 @@ void insertTreeMap(TreeMap * tree, void* key, void * value)
 
 TreeNode * minimum(TreeNode * x){
 
-    return NULL;
+    TreeNode * aux = x;
+
+    while (aux->left != NULL) aux = aux->left;
+
+    return aux;
 }
 
 // 5.- Implemente la función void removeNode(TreeMap * tree, TreeNode* node). 
